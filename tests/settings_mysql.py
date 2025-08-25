9aaa17a68add5d34e3bde174594f032dc1c4cbf1
Mitchel Cabuloy <mixxorz@gmail.com>
2025-08-26 07:50:03 +0800

    Fix MySQL

--- tests/settings_mysql.py
+++ tests/settings_mysql.py
@@ -8,8 +8,10 @@ DATABASES = {
         "NAME": os.environ.get("MYSQL_DATABASE", "isekai_test"),
         "USER": os.environ.get("MYSQL_USER", "root"),
         "PASSWORD": os.environ.get("MYSQL_PASSWORD", "isekai_root"),
-        "HOST": os.environ.get("MYSQL_HOST", "localhost"),
-        "PORT": os.environ.get("MYSQL_PORT", "3306"),
+        "HOST": os.environ.get(
+            "MYSQL_HOST", "127.0.0.1"
+        ),  # Force IP instead of localhost
+        "PORT": int(os.environ.get("MYSQL_PORT", "3306")),
         "OPTIONS": {
             "charset": "utf8mb4",
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
